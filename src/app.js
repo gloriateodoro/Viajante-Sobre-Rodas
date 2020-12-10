70d12c2c4584ce8dfebb289ce0688e09f245f2d2
gloriateodoro <gloriateodoro1@gmail.com>
2020-12-10 01:49:54 -0300

    app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
 
-const mongoose = require('mongoose');
-mongoose.connect(`${process.env.DATABASE}`, 
+
+mongoose.connect('mongodb://localhost:27017/viajanteSobreRodas',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -10,8 +11,12 @@ mongoose.connect(`${process.env.DATABASE}`,
 .then(x => console.log(`Connected to mongo Database name "${x.connections[0].name}"`))
 .catch(err => console.error('Error connecting to mongo', err))
 
-const hotels = require("./routes/hotelRoutes")
+const hostingRoutes = require("./routes/hostingRoutes")
 
-app.use("/", hotels)
+app.use('/api/hosting', hostingRoutes);
+
+app.get("/", (request, response) =>{
+    response.send("Deu certo")
+})
 
 module.exports = app
