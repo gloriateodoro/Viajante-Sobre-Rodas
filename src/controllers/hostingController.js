1f47c2edc821eb51fe7848af8a2f8cbdc5df517b
gloriateodoro <gloriateodoro1@gmail.com>
2020-12-10 01:50:15 -0300

    controller de hospedagem

--- src/controllers/hostingController.js
+++ src/controllers/hostingController.js
@@ -1,14 +1,51 @@
 const hosting = require("../models/hosting") 
 const {request, response} = require("express")
+const e = require("express")
 
-const getByState = (request, response) => {
-    console.log(request.url)
-    return response.status(200).json(hosting)    
+
+/* Mostrar todas as hospedagens por estado */
+const getByState = async(request, response) => {
+
+    try{
+        hosting.find({state: request.params.state})
+        .then(hostingFound =>{
+            response.status(200).send({message: "Nesse estado, essas são as opções de hospedagens com acessibilidade: "
+            , hostingFound})
+        }).catch(e =>{
+            response.status(400).send({message: "Infelizmente, ainda não foram cadastradas hospedagens nesse estado!"})
+        })
+    }catch (error){
+        response.status(400).send({message: error.message})
+    }
+
+    /*try {
+        let hostingFromState = await hosting.find({state: request.params.state});
+        if(hostingFromState) {
+            response.status(200).send(hostingFromState);
+        }else {
+            response.status(400).send({ message: "State not found!" })
+        }
+
+    
+    } catch (error) {
+        response.status(400).send({ message: error.message})
+    }*/
+    
 }
 
-const getByCity = (request, response) =>{
-    /*const cidade = request.params.cidade
-    return response.status(200).send(hotels.find(hotel => hotel.cidade == cidade))*/
+const getByCity = async(request, response) =>{
+    try{
+        hosting.find({city: request.params.city})
+        .then(hostingFound =>{
+            response.status(200).send({message: "Nessa cidade, essas são as opções de hospedagem com acessibilidade: "
+        , hostingFound})
+        }).catch(e =>{
+            response.status(400).send({message: "Infelizmente, ainda não foram cadastradas hospedagens nessa cidade!"})
+        })
+    }catch (error){
+        response.status(400).send({message: error.message})
+    }
+    
 } 
 
 module.exports = {
